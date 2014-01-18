d8c25dffedac5b5ff26e7de142d3d7efe0a066cb
foostan <foostan27@gmail.com>
2014-01-19 00:43:09 +0900

    Add tasks

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,17 +1,16 @@
 /**
  * class Timer
  */
-var Timer = function (id) {
+var Timer = function(id) {
   this.id = id
   this.time_limit = 25 * 60
   this.start_time = null
-};
-
+}
 Timer.prototype = {
-  start: function () {
+  start: function() {
     this.start_time = parseInt((new Date)/1000)
   },
-  reset: function () {
+  reset: function() {
     this.start_time = 0
   },
   getElapsedTime: function() {
@@ -33,13 +32,47 @@ Timer.prototype = {
 }
 
 /**
+ * class Task
+ */
+var Task = function(title, description, state) {
+  this.title = title || null
+  this.description = description || null
+  this.state = state || null
+  this.pomodoro = 0
+  this.toggle = false
+}
+
+/**
+ * class TaskList
+ */
+var TaskList = function() {
+  this.data = []
+}
+TaskList.prototype = {
+  load: function() {
+    var _this = this
+    chrome.storage.sync.get("task_list", function(d) {
+      _this.data = d.task_list
+    });
+  },
+  add: function(task) {
+    this.data.push(task)
+  },
+  save: function() {
+    chrome.storage.sync.set({"task_list": this.data});
+  }
+}
+
+/**
  * Request listener
  */
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
-    var id = request.id
-    if (!timers[id]) {
-      timers[id] = new Timer(id)
+    if(request.id !== undefined) {
+      var id = request.id
+      if (!timers[id]) {
+        timers[id] = new Timer(id)
+      }
     }
 
     switch (request.action) {
@@ -53,6 +86,9 @@ chrome.extension.onRequest.addListener(
         break
       case "getTimeLimit":
         sendResponse({time_limit: timers[id].time_limit})
+        break
+      case "getTaskList":
+        sendResponse({task_list: task_list})
         break
       default:
         sendResponse({})
@@ -79,3 +115,11 @@ setInterval(function() {
     }
   }
 }, 1000)
+
+var task_list = new TaskList()
+task_list.load()
+//task_list.add(new Task("test1", "this is test", "today"))
+//task_list.add(new Task("test2", "this is test", "today"))
+//task_list.add(new Task("test3", "this is test", "today"))
+//task_list.add(new Task("test3", "this is test", "today"))
+//task_list.save()
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -5,7 +5,6 @@ var Timer = function (id) {
   this.id = id
   this.jq_object = $("#" + id)
 }
-
 Timer.prototype = {
   updateTime: function (time) {
     this.setTime(time)
@@ -24,6 +23,51 @@ Timer.prototype = {
 }
 
 /**
+ * class TaskList
+ */
+var TaskList = function(task_list) {
+  this.data = task_list
+}
+TaskList.prototype = {
+  view: function() {
+    console.log(this.data)
+    this.data.forEach(function(task, i){
+      var toggle = task.toggle ? "in" : ""
+      console.log(toggle)
+      $("#task_list").append($('<div class="panel panel-default">')
+        .append($('<div class="panel-heading">')
+          .append($('<span class="panel-title">')
+            .append($('<a data-toggle="collapse" data-parent="#accordion" href="#collapse' +  i + '">')
+              .text(task.title)
+            )
+          )
+          .append($('<span class="badge">' + task.pomodoro + '</span>')
+          )
+          .append($('<div class="btn-group pull-right">')
+            .append($('<button type="button" class="btn btn-xs"><span class="glyphicon glyphicon-ok"></span></button>'))
+            .append($('<button type="button" class="btn btn-xs"><span class="glyphicon glyphicon-trash"></span></button>'))
+          )
+        )
+        .append($('<div id="collapse' + i +'" class="panel-collapse collapse ' + toggle + '">')
+          .append($('<div class="panel-body"><p>' + task.description + '</p></div>')
+          )
+          .append($('<hr/>'))
+          .append($('<div>')
+            .append($('<h4 id="timer' + i + '" class="text-danger timer">25:00</h4>'))
+            .append($('<div class="btn-group btn-group-justified">')
+              .append($('<a class="btn btn-primary btn-start" href="#"><span class="glyphicon glyphicon-play-circle"></span> Start</a>'))
+              .append($('<a class="btn btn-success btn-break" href="#"><span class="glyphicon glyphicon-tree-deciduous"></span> Break</a>'))
+              .append($('<a class="btn btn-danger btn-reset" href="#"><span class="glyphicon glyphicon-ban-circle"></span> Reset</a>'))
+            )
+          )
+        )
+      )
+
+    })
+  }
+}
+
+/**
  * Request listener
  */
 chrome.extension.onRequest.addListener(
@@ -32,6 +76,13 @@ chrome.extension.onRequest.addListener(
 
     switch (request.action) {
       case "update_remaining_time":
+        $(".timer").each(function(){
+          if (timers[id] === undefined) {
+            var id = $(this).attr("id")
+            timers[id] = new Timer(id)
+          }
+        })
+
         timers[id].updateTime(request.remaining_time)
         break
       default:
@@ -49,20 +100,30 @@ $('.collapse').collapse({
   toggle: false
 })
 
-$('.btn-start').on("click", function(){
+$(document).on("click", ".btn-start", function(){
   var $timer = $(this).parent().parent().find(".timer")
   id = $timer.attr("id")
 
   chrome.extension.sendRequest({id: id, action: "start"})
 })
 
-$('.btn-reset').on("click", function(){
+$(document).on("click", ".btn-reset", function(){
   var $timer = $(this).parent().parent().find(".timer")
   id = $timer.attr("id")
 
   timers[id].resetTime()
   chrome.extension.sendRequest({id: id, action: "reset"})
 })
+
+/**
+ * function
+ */
+function updateTaskList() {
+  chrome.extension.sendRequest({action: "getTaskList"}, function(response){
+    var task_list = new TaskList(response.task_list.data)
+    task_list.view()
+  })
+}
 
 /**
  * main
@@ -72,4 +133,6 @@ $(".timer").each(function(){
   var id = $(this).attr("id")
   timers[id] = new Timer(id)
 })
+
+updateTaskList()
 
