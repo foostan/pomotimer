f5615dec5fdb5fffc5b2d3c1826fe9e18abf87ba
foostan <foostan27@gmail.com>
2014-02-10 00:46:51 +0900

    move items to the controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('angularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+.controller('MainCtrl', function ($scope) {
+    $scope.items = [
+        {
+            name: 'HTML5 Boilerplate',
+            dist: 'HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.'
+        },
+        {
+            name: 'AngularJS',
+            dist: 'AngularJS is a toolset for building the framework most suited to your application development.'
+        },
+        {
+            name: 'Karma',
+            dist: 'Spectacular Test Runner for JavaScript.'
+        }
     ];
-  });
+});
