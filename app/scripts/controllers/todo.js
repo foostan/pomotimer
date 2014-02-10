eec9ec5ad9966cb774752fb6e8b8ac5e8a123fa6
foostan <foostan27@gmail.com>
2014-02-11 01:44:36 +0900

    Setup todo template

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -1,19 +1,17 @@
 'use strict';
 
-angular.module('angularApp')
-.controller('TodoCtrl', function ($scope) {
-    $scope.items = [
-        {
-            name: 'HTML5 Boilerplate',
-            dist: 'HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.'
-        },
-        {
-            name: 'AngularJS',
-            dist: 'AngularJS is a toolset for building the framework most suited to your application development.'
-        },
-        {
-            name: 'Karma',
-            dist: 'Spectacular Test Runner for JavaScript.'
-        }
-    ];
-});
+
+function TodoCtrl($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.tasks = [
+    {
+      title: "Dynamic Group Header - 1",
+      content: "Dynamic Group Body - 1"
+    },
+    {
+      title: "Dynamic Group Header - 2",
+      content: "Dynamic Group Body - 2"
+    }
+  ];
+}
