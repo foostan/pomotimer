96f321ef2203dba9982a5598d3f5a0e2b49deb10
foostan <foostan27@gmail.com>
2014-02-16 00:58:30 +0900

    Fix ui/ux

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -6,22 +6,42 @@ function TodoCtrl($scope) {
 
   $scope.tasks = [
     {
-      title: 'sample task - 1',
-      content: 'sample task - 1',
+      title: 'sample task',
+      pomodoro_total: 1,
+      pomodoro_today: 1,
       done: true,
       collapsed: 'isCollapsed'
     },
     {
-      title: 'sample task - 2',
-      content: 'sample task - 2',
+      title: 'sample task',
+      pomodoro_total: 5,
+      pomodoro_today: 1,
       done: true,
       collapsed: '!isCollapsed'
     },
     {
-      title: 'sample task - 3',
-      content: 'sample task - 3',
+      title: 'sample task',
+      pomodoro_total: 2,
+      pomodoro_today: 1,
       done: false,
       collapsed: '!isCollapsed'
     }
   ];
+
+  $scope.checkTaskPomodoroToday = function(task, data) {
+    if (data == null || data == NaN) {
+      return "Please input a number";
+    } else if (data > task.pomodoro_total) {
+      return "Please input " + task.pomodoro_total + " or less";
+    }
+  };
+
+  $scope.checkTaskPomodoroTotal = function(task, data) {
+    if (data == null || data == NaN) {
+      return "Please input a number";
+    } else if (data < task.pomodoro_today) {
+      return "Please input " + task.pomodoro_total + " or over";
+    }
+  };
+
 }
