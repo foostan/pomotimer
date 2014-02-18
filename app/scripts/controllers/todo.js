006e53e3b0b655f2cfba2a8f896214f592b8fe90
foostan <foostan27@gmail.com>
2014-02-19 00:03:06 +0900

    Show pomodoro count and fix validation

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -46,15 +46,18 @@ function TodoCtrl($scope, $interval, TaskManager, PomodoroTimer) {
   $scope.checkCountToday = function(task, data) {
     if (data == null || data == NaN) {
       return "Please input a number";
-    } else if (data > task.count_total) {
+    } else if (data > task.count_total || data < 0) {
       return "Please input " + task.count_total + " or less";
     }
   };
 
   $scope.checkCountTotal = function(task, data) {
+      console.log(task.count_today)
+      console.log(task.count_total)
+      console.log(data)
     if (data == null || data == NaN) {
       return "Please input a number";
-    } else if (data < task.count_today) {
+    } else if (data < task.count_today || data < 0) {
       return "Please input " + task.count_total + " or over";
     }
   };
