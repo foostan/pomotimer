ef9f0bec08ae97a936b7bc87b4cf69f530e458ec
foostan <foostan27@gmail.com>
2014-03-07 00:50:39 +0900

    Fix indents

--- app/scripts/services/pomodoroTimer.js
+++ app/scripts/services/pomodoroTimer.js
@@ -1,42 +1,46 @@
 'use strict';
 
-app.factory("PomodoroTimer", function() {
+app.factory("PomodoroTimer", function () {
     var POMODORO_TIME = 10;
     var time = POMODORO_TIME;
     var target_task = null;
-  var start_time = null;
-  var isRunning = false;
+    var start_time = null;
+    var isRunning = false;
 
-  return {
-    getTime: function() { 
-      if (start_time == null) {
-        return POMODORO_TIME;
-      } else {
-        var now = parseInt((new Date)/1000);
-        return POMODORO_TIME - (now - start_time)
-      }
-    },
-    getStartTime: function() { return start_time; },
-    isRunning: function() { return isRunning; },
-    isFinished: function() {
-      return isRunning && this.getTime() <= 0 ? true : false;
-    },
-    start: function(task) {
-      target_task = task;
-      start_time = parseInt((new Date)/1000);
-      isRunning = true;
-    },
-    stop: function(task) {
-      target_task = null;
-      start_time = null;
-      isRunning = false;
-    },
-    finish: function(task) {
-      this.stop(task);
-    },
-    reset: function(task) {
-      time = POMODORO_TIME;
-      target_task = task;
-    }
-  };
+    return {
+        getTime: function () {
+            if (start_time == null) {
+                return POMODORO_TIME;
+            } else {
+                var now = parseInt((new Date) / 1000);
+                return POMODORO_TIME - (now - start_time)
+            }
+        },
+        getStartTime: function () {
+            return start_time;
+        },
+        isRunning: function () {
+            return isRunning;
+        },
+        isFinished: function () {
+            return isRunning && this.getTime() <= 0 ? true : false;
+        },
+        start: function (task) {
+            target_task = task;
+            start_time = parseInt((new Date) / 1000);
+            isRunning = true;
+        },
+        stop: function (task) {
+            target_task = null;
+            start_time = null;
+            isRunning = false;
+        },
+        finish: function (task) {
+            this.stop(task);
+        },
+        reset: function (task) {
+            time = POMODORO_TIME;
+            target_task = task;
+        }
+    };
 });
