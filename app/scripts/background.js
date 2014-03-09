4030ffc7b30b2dd808ea0a3be220a3825d0b5ea4
foostan <foostan27@gmail.com>
2014-03-09 23:46:46 +0900

    Count time on background

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,33 +1,46 @@
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-    var timeout = null;
+    var running = null;
     switch (request.action) {
         case "timer-start":
-            timeout = setTimeout(function () {
-                chrome.notifications.create('pomotimer', {
-                    type: "basic",
-                    title: "Pomodoro finish!",
-                    message: request.task.title,
-                    contextMessage: "contextMessage",
-                    iconUrl: "images/icon128_kaku.png"
-                }, function () {});
+            var time = request.time;
+            running = setInterval(function () {
+                if (--time > 0) {
+                    chrome.extension.sendRequest({
+                        action: 'timer-running',
+                        task: request.task,
+                        time: time
+                    }, function (req) {
+                    });
 
-                chrome.extension.sendRequest({
-                    action: 'timer-stop',
-                    task: request.task
-                }, function (req) {
-                    if (req == null) {
-                        tasks = JSON.parse(localStorage.getItem('pomotimer') || '[]');
-                        tasks.forEach(function (task) {
-                            if (task.$$hashKey == request.task.$$hashKey) {
-                                task.count_today++;
-                                task.count_total++;
-                            }
-                        });
-                        localStorage.setItem('pomotimer', JSON.stringify(tasks));
-                        console.log("Finished and updated!");
-                    }
-                });
-            }, request.time * 1000);
+                } else {
+                    chrome.notifications.create('pomotimer', {
+                        type: "basic",
+                        title: "Pomodoro finish!",
+                        message: request.task.title,
+                        contextMessage: "contextMessage",
+                        iconUrl: "images/icon128_kaku.png"
+                    }, function () {
+                    });
+
+                    chrome.extension.sendRequest({
+                        action: 'timer-stop',
+                        task: request.task
+                    }, function (req) {
+                        if (req == null) {
+                            tasks = JSON.parse(localStorage.getItem('pomotimer') || '[]');
+                            tasks.forEach(function (task) {
+                                if (task.$$hashKey == request.task.$$hashKey) {
+                                    task.count_today++;
+                                    task.count_total++;
+                                }
+                            });
+                            localStorage.setItem('pomotimer', JSON.stringify(tasks));
+                            console.log("Finished and updated!");
+                        }
+                    });
+                    clearInterval(running);
+                }
+            }, 1000);
             break;
         default:
             break;
